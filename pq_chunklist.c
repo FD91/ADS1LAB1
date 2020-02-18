d31e687d51a7da501b4ca4106f7111c48983b5b3
Christoffer Ivarsson Orrelid <58771651+ToffeIO@users.noreply.github.com>
2020-02-18 11:23:46 +0100

    Update pq_chunklist.c

--- pq_chunklist.c
+++ pq_chunklist.c
@@ -1,27 +1,34 @@
 /*
- * File: pqueue.c
- * --------------
- * Den här filen implementerar en prioritetskö med en
- * vektor med konstant storlek. Implementeringen gör det lätt
- * att sätta in nya element med svårt att plocka ut det största.
- *
- * Cecilia Sönströd, Algoritmer och Datastrukturer 1, vt2020
- */
+* File: pqueue.c
+* --------------
+* Den här filen implementerar en prioritetskö med en
+* vektor med konstant storlek. Implementeringen gör det lätt
+* att sätta in nya element med svårt att plocka ut det största.
+*
+* Cecilia Sönströd, Algoritmer och Datastrukturer 1, vt2020
+*/
 
 #include "pqueue.h"
 #include "genlib.h"
 
- /* Constant: MAX_ELEMENTS
-  * ----------------------
-  * Den här konstanten anger antalet element i den vektor som
-  * utgör representationen av prioritetskön.
-  */
+/* Constant: MAX_ELEMENTS
+* ----------------------
+* Den här konstanten anger antalet element i den vektor som
+* utgör representationen av prioritetskön.
+*/
 
-#define MAX_ELEMS_PER_BLOCK 3
+#define MAX_ELEMS_PER_BLOCK 100
+
+/* Prototypes for static functions
+ * cellT creatCellT(int newValue);
+ * void splitCellT(cellT oldNode); 
+ */
+
+
 
 typedef struct cellT {
 	int nElements;
-	int* array;
+	int array[MAX_ELEMS_PER_BLOCK];
 	struct cellT* link;
 } *cellT;
 
@@ -29,13 +36,22 @@ struct pqueueCDT {
 	cellT head;
 };
 
-static splitCellT(cellT oldNode) {
+static cellT creatCellT(int newValue) {
+	cellT newNode;
+
+	newNode = New(cellT);
+	newNode->nElements = 1;
+	newNode->array[0] = newValue;
+	newNode->link = NULL;
+}
+
+
+static void splitCellT(cellT oldNode) {
 	cellT newNode;
 	int i, j;
 
 	newNode = New(cellT);
 	newNode->nElements = 1;
-	newNode->array = NewArray(MAX_ELEMS_PER_BLOCK, int);
 
 	j = 0;
 	for (i = MAX_ELEMS_PER_BLOCK / 2; i < MAX_ELEMS_PER_BLOCK; i++) {
@@ -44,7 +60,7 @@ static splitCellT(cellT oldNode) {
 	}
 	newNode->link = oldNode->link;
 	oldNode->link = newNode;
-	
+
 	oldNode->nElements = MAX_ELEMS_PER_BLOCK / 2;
 	newNode->nElements = MAX_ELEMS_PER_BLOCK - oldNode->nElements;
 }
@@ -84,12 +100,7 @@ void Enqueue(pqueueADT pqueue, int newValue)
 	lastNode = NULL;
 
 	if (IsEmpty(pqueue)) {
-		// Lägga in i egen funktion?
-		newNode = New(cellT);
-		newNode->nElements = 1;
-		newNode->array = NewArray(MAX_ELEMS_PER_BLOCK, int);
-		newNode->array[0] = newValue;
-		newNode->link = NULL;
+		newNode = creatCellT(newValue);
 		pqueue->head = newNode;
 	}
 	else {
@@ -117,26 +128,21 @@ void Enqueue(pqueueADT pqueue, int newValue)
 				currentNode = currentNode->link;
 			}
 		}
-		// Lägga in i egen funktion?
-		newNode = New(cellT);
-		newNode->nElements = 1;
-		newNode->array = NewArray(MAX_ELEMS_PER_BLOCK, int);
-		newNode->array[0] = newValue;
-		newNode->link = NULL;
+		newNode = creatCellT(newValue);
 		lastNode->link = newNode;
 	}
 }
 
 /*
- * Implementation notes: DequeueMax
- * -------------------------------------------------
- * Då elementen sparas osorterat i fältet måste en sökning göras
- * för att finna det största elementet. För att ta bort det största
- * elementet flyttas det sista elementet i fältet till den position
- * i vilken det största elementet återfanns samtidigt som antalet
- * element i fältet minskas med 1. Det värde som tas bort returneras från
- * funktionen.
- */
+* Implementation notes: DequeueMax
+* -------------------------------------------------
+* Då elementen sparas osorterat i fältet måste en sökning göras
+* för att finna det största elementet. För att ta bort det största
+* elementet flyttas det sista elementet i fältet till den position
+* i vilken det största elementet återfanns samtidigt som antalet
+* element i fältet minskas med 1. Det värde som tas bort returneras från
+* funktionen.
+*/
 
 int DequeueMax(pqueueADT pqueue)
 {
